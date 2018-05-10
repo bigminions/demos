680f3d63d705b559e2371d094f5b248c936753ae
daren <852694152@qq.com>
2018-05-10 14:19:11 +0800

    test

---
+++ src/main/java/com/githup/bigminions/stream/StreamDemo1.java
@@ -0,0 +1,23 @@
+package com.githup.bigminions.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by daren on 2018/4/28.
+ */
+public class StreamDemo1 {
+
+    @Test
+    public void testSkipAndLimit() {
+        List<Integer> numbers = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> page = numbers.stream().skip(2).limit(2).collect(Collectors.toList());
+        System.out.println(page);
+
+        List<Integer> page2 = numbers.stream().skip(20).limit(2).collect(Collectors.toList());
+        System.out.println(page2);
+
+    }
+}
