ee8c6b9af29154b18a3ccfa81e86939b7269dcd6
daren <852694152@qq.com>
2018-05-17 14:31:01 +0800

    test

---
+++ src/main/java/com/githup/bigminions/example/BigDataTest.java
@@ -0,0 +1,27 @@
+package com.githup.bigminions.example;
+
+import com.google.common.base.Stopwatch;
+import org.junit.Test;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by daren on 2018/5/17.
+ */
+public class BigDataTest {
+
+    @Test
+    public void testMap() {
+        ConcurrentHashMap map = new ConcurrentHashMap();
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        for (int i = 0; i < 1000000; i++) {
+            map.put(i, "value=" + i);
+        }
+        System.out.println("insert 1000000 used " + stopwatch.elapsed(TimeUnit.MILLISECONDS) + " ms");
+        stopwatch.reset();
+        stopwatch.start();
+        System.out.println(map.get(100));;
+        System.out.println("get from 1000000 used " + stopwatch.elapsed(TimeUnit.MILLISECONDS) + " ms");
+    }
+}
