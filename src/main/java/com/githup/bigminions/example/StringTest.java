410228239f89215d940162a6b172bcb9baead5b9
daren <aiyinsitang@gmail.com>
2021-02-18 11:49:10 +0800

    update

---
+++ src/main/java/com/githup/bigminions/example/StringTest.java
@@ -0,0 +1,25 @@
+package com.githup.bigminions.example;
+
+public class StringTest {
+
+    public static void main(String[] args) {
+        String a = "Hello";
+        String b = "Hello";
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a equals b : " + a.equals(b));
+
+
+        String c = "H";
+        String d = "ello";
+        String e = c + d;
+        String f = new String("Hello");
+
+        System.out.println("e == b : " + (e == b));
+        System.out.println("f == b : " + (f == b));
+
+        System.out.println("a.hash = " + a.hashCode());
+        System.out.println("b.hash = " + b.hashCode());
+        System.out.println("e.hash = " + e.hashCode());
+        System.out.println("f.hash = " + f.hashCode());
+    }
+}
